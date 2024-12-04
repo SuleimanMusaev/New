c8aa8ed386319cb7eaed3920aa7a909d14fd47bc
SuleimanMusaev <suli141198@gmail.com>
2024-12-04 21:23:15 +0100

    minor

--- BaseJava/src/main/java/com/TelRun/base/homeWork/_04_12_24/ASKJH.java
+++
@@ -1,8 +0,0 @@
-package com.TelRun.base.homeWork._04_12_24;
-
-public class ASKJH {
-
-    public static void main(String[] args) {
-        int asd =5;
-    }
-}
--- BaseJava/src/main/java/com/TelRun/base/homeWork/_04_12_24/ExampleCloserNumber.java
+++ BaseJava/src/main/java/com/TelRun/base/homeWork/_04_12_24/ExampleCloserNumber.java
@@ -2,22 +2,22 @@ package com.TelRun.base.homeWork._04_12_24;
 
 import java.util.Scanner;
 
-public class ExampleTimer {
+public class ExampleNumber {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("input your first number");
-
+        System.out.println("Input your first number:");
         double m = scanner.nextDouble();
+        System.out.println("Input your second number:");
         double n = scanner.nextDouble();
 
         double distanceM = Math.abs(m - 10);
         double distanceN = Math.abs(n - 10);
 
         if (distanceM < distanceN){
-            System.out.println("Число" + m + "ближе к 10");
+            System.out.println("Number '" + m + "' is closer to 10");
         } else {
-            System.out.println("Число" + n + "ближе к 10");
+            System.out.println("Number '" + n + "' is closer to 10");
         }
     }
 }
